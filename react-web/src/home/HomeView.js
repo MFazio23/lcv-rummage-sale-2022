9dfea26b9c2fa3fd7e3b2eb3cdf06d25de3e6bc4
Michael Fazio <michaelfazio@albert.com>
2022-07-05 23:09:35 -0500

    Update PDF URL

--- react-web/src/home/HomeView.js
+++ react-web/src/home/HomeView.js
@@ -4,7 +4,7 @@ import {FileDownload, FormatListBulleted, Map} from "@mui/icons-material";
 import {Stack} from "@mui/material";
 
 function HomeView(props) {
-    const pdfLink = "https://faziostaticsitestorage.file.core.windows.net/lcv-rummage-sale/LCV%20Rummage%20Sale.pdf?sv=2021-04-10&st=2022-05-30T17%3A27%3A39Z&se=2055-05-31T17%3A27%3A00Z&sr=f&sp=r&sig=P9JQfF7CLi4UJyFgvleT2xJTQGCDYmos26Enndx6UDA%3D";
+    const pdfLink = "https://faziostaticsitestorage.file.core.windows.net/lcv-rummage-sale/Rummage flyer final.pdf?sp=r&st=2022-07-06T04:08:12Z&se=2024-07-07T04:08:00Z&spr=https&sv=2021-06-08&sig=0EvbYKwtS4efZHbGr8CJ80Ak8iEmlDYOZXYxzW%2FeC8Y%3D&sr=f";
 
     return (
         <div style={{padding: '8px'}}>

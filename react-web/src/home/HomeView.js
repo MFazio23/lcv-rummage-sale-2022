35ae38ae885fb66880c32eec383dc36c606725d7
Michael Fazio <mfazio23@gmail.com>
2022-07-07 08:26:39 -0500

    Update PDF link

--- react-web/src/home/HomeView.js
+++ react-web/src/home/HomeView.js
@@ -4,8 +4,7 @@ import {FileDownload, FormatListBulleted, Map} from "@mui/icons-material";
 import {Stack} from "@mui/material";
 
 function HomeView(props) {
-    const pdfLink = "https://faziostaticsitestorage.file.core.windows.net/lcv-rummage-sale/Rummage flyer final.pdf?sp=r&st=2022-07-06T04:08:12Z&se=2024-07-07T04:08:00Z&spr=https&sv=2021-06-08&sig=0EvbYKwtS4efZHbGr8CJ80Ak8iEmlDYOZXYxzW%2FeC8Y%3D&sr=f";
-
+    const pdfLink = "https://faziostaticsitestorage.file.core.windows.net/lcv-rummage-sale/lcv-rummage-flyer.pdf?sv=2021-04-10&st=2022-07-07T13%3A06%3A10Z&se=2024-07-08T13%3A06%3A00Z&sr=f&sp=r&sig=XK%2FyRMRCkEb8%2BTasF5tpeg8uu6Ivxd4Y9HcXRGkCAYM%3D"
     return (
         <div style={{padding: '8px'}}>
             <h1>Lake Country Village Rummage Sale '22</h1>
